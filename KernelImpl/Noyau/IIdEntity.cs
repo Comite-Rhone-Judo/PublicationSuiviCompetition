8ec4f6cb5bd57987e2385f21dbbdca86f2859858
Comite-Rhone-Judo <dgarrivier@free.fr>
2025-11-23 21:10:31 +0100

    Commit modification kernelImpl (corrective branche parallèle)

---
+++ KernelImpl/Noyau/IIdEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KernelImpl.Noyau
+{
+    internal interface IIdEntity<IDType>
+    {
+        IDType id {  get; }
+    }
+}
